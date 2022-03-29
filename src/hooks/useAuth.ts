41eb62e6529b2204747465ab72879d88dd8dd1cc
meteorol50 <meteorol50.sky@gmail.com>
2022-03-29 16:08:08 +0900

    Add Toast Message

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,12 @@ import { useCallback, useState } from "react"
 import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
+
   const [ loding, setLoding ] = useState(false);
 
   const login = useCallback((id: string) => {
@@ -13,14 +16,15 @@ export const useAuth = () => {
 
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if (res.data) {
+        showMessage({ title: 'ログインしました', status: 'success'});
         history.push('/home');
       } else {
-        alert('ユーザーが見つかりません');
+        showMessage({ title: 'ユーザーが見つかりません', status: 'error'});
       }
     })
-    .catch(() => alert('ログインできません'))
+    .catch(() => showMessage({ title: 'ログインできません', status: 'error'}))
     .finally(() => setLoding(false));
 
-  }, [history]);
+  }, [history, showMessage]);
   return { login, loding };
 }
