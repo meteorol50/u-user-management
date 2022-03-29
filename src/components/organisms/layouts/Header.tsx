f5246a6680d4453f93fcfc73ddbf0acd9960311a
meteorol50 <meteorol50.sky@gmail.com>
2022-03-29 15:05:16 +0900

    Divide Header into Components

--- src/components/organisms/layouts/Header.tsx
+++ src/components/organisms/layouts/Header.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, useDisclosure } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Flex, Heading, useDisclosure } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 import { Link } from "react-router-dom";
+import { MenuIconButton } from "../../atoms/button/MenuIconButton";
+import { MenuDrawer } from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -24,26 +25,9 @@ export const Header: VFC = memo(() => {
           </Box>
             <Link to='/home/setting'>設定</Link>
         </Flex>
-        <IconButton
-          aria-label='メニューボタン'
-          icon={<HamburgerIcon />}
-          size='sm'
-          variant='unstyled'
-          display={{ base: 'block', md: 'none' }}
-          onClick={onOpen}
-        />
+        <MenuIconButton {...{ onOpen }} />
       </Flex>
-      <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg='gray.100'>
-              <Button w='100%'>TOP</Button>
-              <Button w='100%'>ユーザー一覧</Button>
-              <Button w='100%'>設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+      <MenuDrawer {...{ onClose, isOpen }} />
     </>
   )
 })
